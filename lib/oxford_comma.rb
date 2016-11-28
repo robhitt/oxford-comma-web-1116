eaffe15c447fdb9393b65bf1a0c9b7a3272d1534
Robert Hitt <robhitt@gmail.com>
2016-11-28 00:33:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
+  return array[0] if array.length == 1
+  return "#{array[0]} and #{array[1]}" if array.length == 2
 
+  last_element = ", and #{array.pop}"
+  array.join(", ") + last_element
 end
